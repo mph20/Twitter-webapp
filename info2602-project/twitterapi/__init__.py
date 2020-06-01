116af82d99edd6c15a07f430afa76de89e372b7c
Marcus Hackshaw <49069492+mph20@users.noreply.github.com>
2020-06-01 06:49:46 -0400

    Update __init__.py

--- info2602-project/twitterapi/__init__.py
+++ info2602-project/twitterapi/__init__.py
@@ -8,8 +8,8 @@ from flask_login import LoginManager
 
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://jrkxwjwgnxxenz:ff62ee491e09f976ca6ec2c7836fce836d8a1bbc24ee04737f6b3bfe7d48bcbb@ec2-23-23-173-30.compute-1.amazonaws.com:5432/dfufntauabctoa'
+app.config['SECRET_KEY'] = 'removedforsecuritypurposes'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'removedforsecuritypurposes'
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 
